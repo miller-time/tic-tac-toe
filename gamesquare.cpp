99584a3bd52dcc67eea84d578ed7ec108be6acb7
millertime <millertime@localhost>
2010-11-19 11:06:59 -0800

    drawing images onto the window yay

--- gamesquare.cpp
+++ gamesquare.cpp
@@ -1,6 +1,8 @@
 #include <QGraphicsScene>
 #include <QPainter>
 #include <QStyleOption>
+#include <QString>
+#include <QPixmap>
 #include "gamesquare.h"
 
 GameSquare::GameSquare()
@@ -14,13 +16,14 @@ QRectF GameSquare::boundingRect() const
 
 void GameSquare::paint(QPainter *painter, const QStyleOptionGraphicsItem *, QWidget *)
 {
-    painter->setBrush(Qt::black);
-    painter->drawEllipse(-200,-200,200,200);
+    QString xFile = "images/x.png";
+    QPixmap xPiece(xFile, 0, Qt::AutoColor);
+    painter->drawPixmap(0,0,xPiece);
 }
 
 QPainterPath GameSquare::shape () const
 {
     QPainterPath path;
-    path.addRect(-200,-200,200,200);
+    path.addRect(-50,-50,50,50);
     return path;
 }
--- main.cpp
+++ main.cpp
@@ -10,13 +10,29 @@ int main(int argc, char *argv[])
 
     // Start a scene
     QGraphicsScene scene;
-    scene.setSceneRect(-300,-300,300,300);
+    scene.setSceneRect(0,0,300,300);
     scene.setItemIndexMethod(QGraphicsScene::NoIndex);
 
     // Make the 9 Tic Tac Toe squares, add them to scene
     GameSquare *square_1 = new GameSquare;
     square_1->setPos(0,0);
     scene.addItem(square_1);
+
+    GameSquare *square_2 = new GameSquare;
+    square_2->setPos(100,0);
+    scene.addItem(square_2);
+
+    GameSquare *square_3 = new GameSquare;
+    square_3->setPos(200,0);
+    scene.addItem(square_3);
+
+    GameSquare *square_4 = new GameSquare;
+    square_4->setPos(0,100);
+    scene.addItem(square_4);
+
+    GameSquare *square_5 = new GameSquare;
+    square_5->setPos(100,100);
+    scene.addItem(square_5);
 
     // Make the view that displays the scene
     QGraphicsView view(&scene);
